897a8e0e66e94f70a5879fa0ea324710b7082d3f
nus-ryan <ryan@nustechnology.com>
2023-01-30 15:34:17 +0700

    Define colors, fonts, breakpoints

---
+++ src/assets/images/index.ts
@@ -0,0 +1,10 @@
+import bbplusLogo from "./logo.png";
+import bbplusFavicon from "./favicon.png";
+
+
+const IMAGES = {
+  bbplusLogo,
+  bbplusFavicon
+}
+
+export default IMAGES;
---
+++ src/utils/breakpoints.ts
@@ -0,0 +1,8 @@
+export const BREAKPOINTS = {
+  mobileSm: "screen and (max-width: 325px)",
+  mobileLg: "screen and (max-width: 475px)",
+  tablet: "screen and (max-width: 755px)",
+  laptop: "screen and (max-width: 1024px)",
+  desktopSm: "screen and (max-width: 1024px)",
+  desktopLg: "screen and (max-width: 1024px)",
+}
