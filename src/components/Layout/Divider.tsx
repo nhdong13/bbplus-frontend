eb19d9e02243262a92252d882de4684e10544684
nus-ryan <ryan@nustechnology.com>
2023-02-01 14:06:28 +0700

    Added: Footer, Header, Layouts

---
+++ src/components/Layout/Divider.tsx
@@ -0,0 +1,30 @@
+import { COLORS } from "@/utils/colors"
+import styled from "styled-components"
+
+const StyledDivider = styled.div.attrs((props: {
+  color?: string
+  height?: string
+  width?: string
+}) => props)`
+  background: ${props => props.color || COLORS.white};
+  height: ${props => props.height};
+  width: ${props => props.width};
+`
+
+interface Divider {
+  color?: string
+  height?: string
+  width?: string
+}
+
+export default function Divider({
+  color,
+  height,
+  width,
+}: Divider) {
+  return (
+    <>
+      <StyledDivider color={color} height={height} width={width} />
+    </>
+  )
+}
